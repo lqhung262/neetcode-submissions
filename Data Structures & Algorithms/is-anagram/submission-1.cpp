fcc1a4957a6dfa60b6111871e7fa9bb8b424cfba
Lê Quốc Hưng <66760866+lqhung262@users.noreply.github.com>
2026-06-07 23:06:52 +0700

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+
+        unordered_map<char, int> um;
+        for (int i=0; i<s.length(); i++)
+        {
+            um[s[i]]++;
+            um[t[i]]--;
+        }
+
+        for (auto it: um)
+        {
+            if (it.second != 0)
+                return false;
+        }
+
+        return true;
+    }
+};
